ec7b75bbff92d144ac639a4641bd6ebfd999c015
MARTHA J CHAMBERLIN <90939439+marthachamberlin@users.noreply.github.com>
2021-10-28 10:31:11 -0700

    Add files via upload

---
+++ scores_functions.cpp
@@ -0,0 +1,9 @@
+void ReadScores(float cStu[])
+{
+    for (int i = 0; i < NUM_SCORES; i++)
+    {
+        cout << "Enter score #" << i + 1 << " of " << NUM_SCORES << ": ";
+        cin >> cStu[i];
+    }
+}
+

4cda19430f3b47f881f5e3d3fa89a5d8faecc06a
JordanHT-OIT <jordan.harristoovy@oit.edu>
2021-11-03 17:41:36 -0700

    Second test commit

---
+++ Project2_Ansari_Harris-Toovy/primaryHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+//Code by Jordan Harris-Toovy and Rayyan Ansari for OIT's CST116-01P project 2, November 2021
+
+#include <iostream>
+#include <iomanip>
+
+#define NUM_SCORES 5
+
+using namespace std;
+
+//Function 1 - Get scores 
+void ReadScores(float[]);
